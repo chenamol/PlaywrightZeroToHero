5aeb9563cd0b97e63c14159c0f5ba7fa5d6b8085
Cesar Henao <k-cesar.henao@kaseya.com>
2025-05-05 10:13:51 -0500

    playwrightImplementation

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,11 @@
+import {test} from '@playwright/test'
+
+test.beforeEach(async ({page}) => {
+   await page.goto('http://localhost:4200/')
+   await page.getByText('Forms').click()
+   await page.getByText('Form Layouts').click()
+
+})
+
+
+
